9956a502b453e5b54b4c2ca700793f9fc2d72c2a
Júnior Nascimento <krymancer@protonmail.com>
2025-07-18 00:13:06 -0300

    Add initial backend setup with Docker, Nginx, and PostgreSQL configuration
    - Created Dockerfile for building and running the backend application.
    - Added docker-compose.yml for orchestrating backend services and database.
    - Implemented Nginx configuration for reverse proxying.
    - Set up PostgreSQL database with initial schema and connection settings.
    - Developed basic API endpoints in Program.cs for health check and payments.
    - Configured appsettings.json for environment and connection strings.
    - Included .gitignore for Visual Studio and build artifacts.

---
+++ backend/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Npgsql;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, JsonContext.Default);
+});
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddSingleton<NpgsqlConnection>(sp =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("default");
+    return new NpgsqlConnection(connectionString);
+});
+
+builder.Services.AddHttpClient("default", client =>
+{
+  client.BaseAddress = new Uri(builder.Configuration.GetConnectionString("default"));
+});
+
+builder.Services.AddHttpClient("fallback", client =>
+{
+  client.BaseAddress = new Uri(builder.Configuration.GetConnectionString("fallback"));
+});
+
+var app = builder.Build();
+
+app.MapGet('/healthcheck', async () => 
+{
+  return Results.Ok("healthy");
+});
+
+app.MapGet('/payments-summary', async() => 
+{
+  return Results.Ok("TODO");
+});
+
+app.MapPost('/payments', async () => 
+{
+  return Results.Ok("TODO");
+});
+
+await app.RunAsync();
+
+[JsonSerializable(typeof(int))]
+[JsonSerializable(typeof(char))]
+[JsonSerializable(typeof(DateTime))]
+public partial class JsonContext : JsonSerializerContext { }
